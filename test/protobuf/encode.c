43c3f7b6f1a905be585cde027bc0d399199b69c5
fantasyni <fantasyni@163.com>
2013-02-06 11:42:51 +0800

    review

--- test/protobuf/encode.c
+++ test/protobuf/encode.c
@@ -4,39 +4,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(){
+int main() {
 
     json_t *msg, *protos;
     json_error_t error;
 
     msg = json_load_file("test.json", 0, &error);
 
-    if(!msg){
-    	printf("error load msg json\n");
-    	return 0;
+    if (!msg) {
+        printf("error load msg json\n");
+        return 0;
     }
 
     protos = json_load_file("test.proto", 0, &error);
 
-    if(!protos){
-    	printf("error load protos json\n");
-    	return 0;
+    if (!protos) {
+        printf("error load protos json\n");
+        return 0;
     }
 
     uint8_t buffer[512];
     //memset(buffer,0,sizeof(buffer));
     //pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
     size_t written = 0;
-    if(!pc_pb_encode(buffer,sizeof(buffer),&written,protos,msg)){
-    	printf("pb_encode error\n");
-    	return 0;
+    if (!pc_pb_encode(buffer, sizeof(buffer), &written, protos, msg)) {
+        printf("pb_encode error\n");
+        return 0;
     }
 
     fwrite(buffer, 1, written, stdout);
     /*int i;
-    for(i=0;i<stream.bytes_written;i++){
-        printf("%x", buffer[i]);
-    }*/	
+     for(i=0;i<stream.bytes_written;i++){
+     printf("%x", buffer[i]);
+     }*/
     printf("\n");
     //printf("%s\n", buffer);
 }
--- test/protobuf/tcp_encode.c
+++ test/protobuf/tcp_encode.c
@@ -9,8 +9,8 @@
 #define NUM_WRITE_REQS  1
 
 typedef struct {
-  uv_write_t req;
-  uv_buf_t buf;
+    uv_write_t req;
+    uv_buf_t buf;
 } write_req;
 
 uv_loop_t *loop;
@@ -19,14 +19,14 @@ uv_tcp_t client;
 uv_connect_t req;
 uv_shutdown_t shutdown_req;
 
-void on_connect(uv_connect_t* req,int status);
-void on_write(uv_write_t* req,int status);
+void on_connect(uv_connect_t* req, int status);
+void on_write(uv_write_t* req, int status);
 void on_read(uv_stream_t* tcp, ssize_t nread, uv_buf_t buf);
 void on_shutdown(uv_shutdown_t* req, int status);
 void on_close(uv_handle_t* handle);
 uv_buf_t on_alloc(uv_handle_t* handle, size_t suggested_size);
- 
-void on_connect(uv_connect_t* req,int status){
+
+void on_connect(uv_connect_t* req, int status) {
     printf("connect to tcp\n");
     int i;
     int r;
@@ -48,30 +48,31 @@ void on_connect(uv_connect_t* req,int status){
 }
 
 uv_buf_t on_alloc(uv_handle_t* handle, size_t suggested_size) {
-  static char slab[65536];
-  return uv_buf_init(slab, sizeof slab);
+    static char slab[65536];
+    return uv_buf_init(slab, sizeof slab);
 }
 
 void on_read(uv_stream_t* tcp, ssize_t nread, uv_buf_t buf) {
     if (nread == -1) {
         if (uv_last_error(loop).code != UV_EOF)
-            fprintf(stderr, "Read error %s\n", uv_err_name(uv_last_error(loop)));
-        uv_close((uv_handle_t*) tcp, NULL);
+            fprintf(stderr, "Read error %s\n",
+                    uv_err_name(uv_last_error(loop)));
+        uv_close((uv_handle_t*) tcp, NULL );
         return;
     }
     printf("read\n");
     printf("%d\n", buf.len);
-    printf("%s\n", buf.base); 
+    printf("%s\n", buf.base);
     char* text = buf.base;
     json_error_t error;
-    json_t *data,*x,*y,*test;
+    json_t *data, *x, *y, *test;
     data = json_loads(text, 0, &error);
-    if(!json_is_object(data)){
-        return ;
+    if (!json_is_object(data)) {
+        return;
     }
     x = json_object_get(data, "x");
-    y = json_object_get(data,"y");
-    test = json_object_get(data,"test");
+    y = json_object_get(data, "y");
+    test = json_object_get(data, "test");
 
     printf("x is %" JSON_INTEGER_FORMAT "\n", json_integer_value(x));
     printf("y is %" JSON_INTEGER_FORMAT "\n", json_integer_value(y));
@@ -79,7 +80,7 @@ void on_read(uv_stream_t* tcp, ssize_t nread, uv_buf_t buf) {
     //uv_close((uv_handle_t*)tcp, on_close);
 }
 
-void on_write(uv_write_t* req,int status){
+void on_write(uv_write_t* req, int status) {
     if (status == -1) {
         fprintf(stderr, "Write error %s\n", uv_err_name(uv_last_error(loop)));
     }
@@ -87,12 +88,12 @@ void on_write(uv_write_t* req,int status){
     //free((write_req*)req);
 }
 
-void on_shutdown(uv_shutdown_t* req, int status){
+void on_shutdown(uv_shutdown_t* req, int status) {
     //uv_close((uv_handle_t*)req->handle, on_close);
     free(write_reqs);
 }
 
-void on_close(uv_handle_t* handle){
+void on_close(uv_handle_t* handle) {
     printf("close it\n");
 }
 
@@ -103,14 +104,14 @@ int main() {
 
     msg = json_load_file("test.json", 0, &error);
 
-    if(!msg){
+    if (!msg) {
         printf("error load msg json\n");
         return 0;
     }
 
     protos = json_load_file("test.proto", 0, &error);
 
-    if(!protos){
+    if (!protos) {
         printf("error load protos json\n");
         return 0;
     }
@@ -119,7 +120,7 @@ int main() {
     //memset(buffer,0,sizeof(buffer));
     //pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
     size_t written = 0;
-    if(!pc_pb_encode(buffer,sizeof(buffer),&written,protos,msg)){
+    if (!pc_pb_encode(buffer, sizeof(buffer), &written, protos, msg)) {
         printf("pb_encode error\n");
         return 0;
     }
@@ -128,14 +129,13 @@ int main() {
     write_reqs = malloc(sizeof(*write_reqs) * NUM_WRITE_REQS);
     int i;
     for (i = 0; i < NUM_WRITE_REQS; i++) {
-        write_reqs[i].buf = uv_buf_init(buffer,
-                                        written);
+        write_reqs[i].buf = uv_buf_init(buffer, written);
     }
     uv_tcp_init(loop, &client);
     //uv_tcp_keepalive(&client,1,1);
     //uv_tcp_nodelay(&client,1);
     struct sockaddr_in connect_addr = uv_ip4_addr("0.0.0.0", 7000);
-    uv_tcp_connect(&req,&client,connect_addr,on_connect);
+    uv_tcp_connect(&req, &client, connect_addr, on_connect);
     uv_run(loop);
 
     return 0;
